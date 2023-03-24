90709c902ff80d38384e5e53f7d01477c54bf59c
Insoo Kim <insoo@hotmail.com>
2023-03-24 14:22:12 +0900

    tested 00-01

--- Chapter04/programs/prog01.py
+++ Chapter04/programs/prog01.py
@@ -3,7 +3,8 @@ img = cv2.imread('/home/pi/book/dataset/4.2.03.tiff', 1)
 cv2.imshow('Mandrill', img)
 keyPress = cv2.waitKey(0)
 if keyPress == ord('q'):
-    cv2.destroyWindow('Mandrill')
+    #cv2.destroyWindow('Mandrill')
+    cv2.destroyAllWindows()
 elif keyPress == ord('s'):
     cv2.imwrite('test.jpg', img)
     cv2.destroyWindow('Mandrill')
