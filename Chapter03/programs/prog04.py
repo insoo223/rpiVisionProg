66a16f676010964a5f5ccdd8b65998039c497941
Insoo Kim <insoo@hotmail.com>
2023-03-24 13:07:40 +0900

    tested by me

--- Chapter03/programs/prog04.py
+++ Chapter03/programs/prog04.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array([[0, 1, 2, 3, 4],
-              [5, 6, 7, 8, 9]], dtype = np.uint8 )
+#x = np.array([[0, 1, 2, 3, 4],
+#              [5, 6, 7, 8, 9]], dtype = np.uint8 )
 
-plt.imshow(x, cmap='Accent')
-
+x = np.random.rand(3,3,3)
+plt.imshow(x)
+#plt.imshow(x, cmap='Accent') # Green to colorful to Blk
+#plt.imshow(x, cmap='spring')
+#plt.imshow(x) # Blk to yellow
+#plt.imshow(x, cmap='gray') # Blk to gray
 plt.axis('off')
 plt.grid('off')
 
 plt.show()
+
+#print(plt.colormaps())
